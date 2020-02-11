cf9f2380efe5978154e8a02046517782d5783fd3
Paul Johnson <paul@johnson.kiwi.nz>
2020-02-10 21:49:55 -0800

    Backing up before more setting up

--- dist/lib/file_backend.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class FileBackend {
-    constructor(fileName) {
-        this.fileName = fileName;
-    }
-}
-exports.FileBackend = FileBackend;
--- dist/server/schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var GQLFieldTypes;
-(function (GQLFieldTypes) {
-    GQLFieldTypes["StringType"] = "stringType";
-    GQLFieldTypes["BooleanType"] = "booleanType";
-    GQLFieldTypes["IntType"] = "intType";
-    GQLFieldTypes["FloatType"] = "floatType";
-})(GQLFieldTypes = exports.GQLFieldTypes || (exports.GQLFieldTypes = {}));
